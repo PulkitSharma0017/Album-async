08ebfcb264998ab1c5ff8e3ec10328e4f898c8ab
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-09-11 03:00:45 +0530

    display data using thunks

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList() {
-  return <div>UsersList</div>;
+  const dispatch = useDispatch();
+
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error Fetching Data...</div>;
+  }
+
+  if (error) {
+    return <div>Error Fetching Data...</div>;
+  }
+
+  return (
+    <div>
+      <div>{data.length}</div>
+    </div>
+  );
 }
 
 export default UsersList;
