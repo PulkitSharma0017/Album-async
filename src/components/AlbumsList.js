60686d2757e7eed0701ddbd43d9346e6a3d4abf1
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-09-25 21:29:03 +0530

    add minor styling

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -14,7 +14,7 @@ function AlbumsList({ user }) {
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error Loading albums.</div>;
   } else {
@@ -30,9 +30,11 @@ function AlbumsList({ user }) {
 
   return (
     <div>
-      <div>
-        Albums By {user.name}
-        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums By {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
       </div>
       <div>{content}</div>
     </div>
