f2b9bdd082b12b0ca7449802f316b6257bbceb53
PulkitSharma0017 <125790209+PulkitSharma0017@users.noreply.github.com>
2023-09-25 19:45:13 +0530

    display list of albums and add album button

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,42 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
+  const [addAlbum, results] = useAddAlbumMutation();
 
-  return <div>Albums For {user.name}</div>;
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error Loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>
+        Albums By {user.name}
+        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
